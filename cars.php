1b41317b89bc60d8d43cc1653e7884e408df56ee
Oksana Alekseienko <oksana.alekseienko@gmail.com>
2020-03-24 12:39:59 +0200

    використання switch

--- cars.php
+++ cars.php
@@ -1,14 +1,35 @@
 <?php
 
-if( $_GET['action'] === 'car' && $_POST['count'] < 5 )
+//if( $_GET['action'] === 'car' && $_POST['count'] < 5 )
+//{
+//    echo "Додано авто в кількості до 5 шт";
+//}
+//elseif( $_GET['action'] === 'car' && $_POST['count'] < 10 )
+//{
+//    echo "Додано авто в кількості до 10 шт";
+//}
+//else
+//{
+//    echo "Запис не додано";
+//}
+
+
+$action = $_GET['action'];
+$brand  = $_POST['brand'];
+$price  = $_POST['price'];
+$count  = $_POST['count'];
+
+switch($count)
 {
-    echo "Додано авто в кількості до 5 шт";
-}
-elseif( $_GET['action'] === 'car' && $_POST['count'] < 10 )
-{
-    echo "Додано авто в кількості до 10 шт";
-}
-else
-{
-    echo "Запис не додано";
+    case ( $action === 'car' && $count < 5 ):
+        echo "Додано авто в кількості до 5 шт";
+        break;
+
+    case ( $action === 'car' && $count < 10 ):
+        echo "Додано авто в кількості до 10 шт";
+        break;
+
+    case ( !isset($brand) && !isset($price) && !isset($count) ):
+        echo "Запис не додано";
+        break;
 }
