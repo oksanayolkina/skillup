db3c230199afbe4017cbae4463844b0e97556bfc
Oksana Alekseienko <oksana.alekseienko@gmail.com>
2020-03-25 19:02:11 +0200

    змінено if-else

--- cars.php
+++ cars.php
@@ -1,15 +1,35 @@
 <?php
 
 $action = $_GET['action'];
-$brand  = $_POST['brand'];
-$price  = $_POST['price'];
-$count  = $_POST['count'];
+$brand  = $_POST['brand'] ?? '';
+$price  = $_POST['price'] ?? '';
+$count  = $_POST['count'] ?? ''; // isset($_POST['count']) ? $_POST['count'] : '';
 
-if( !empty( $brand ) && !empty( $price ) && !empty( $count ) )
+//if( !empty( $brand ) && !empty( $price ) && !empty( $count ) )
+//{
+//    echo "Додано авто в кількості " . $count . " шт";
+//}
+//elseif( empty( $brand ) && empty( $price ) && empty( $count ) )
+//{
+//    echo "Не додано";
+//}
+//else
+//{
+//    echo "Щось трапилось";
+//}
+
+
+switch( true )
 {
-    echo "Додано авто в кількості " . $count . " шт";
-}
-else
-{
-    echo "Щось трапилось";
+    case ( !empty( $brand ) && !empty( $price ) && !empty( $count ) ):
+        echo "Додано авто в кількості " . $count . " шт";
+        break;
+
+    case ( empty( $brand ) && empty( $price ) && empty( $count ) ):
+        echo "Не додано";
+        break;
+
+    default:
+        echo "Щось трапилось";
+        break;
 }
