f09c639e0bc7d38958f44d20dff2632567537fa0
Oksana Alekseienko <oksana.alekseienko@gmail.com>
2020-04-01 11:19:02 +0300

    щось

--- php_cycles.php
+++ php_cycles.php
@@ -1,7 +1,7 @@
 <?php
 
 $arr_en = range( 'a', 'z' );
-$len    = count( $arr_en );
+$len    = count( $arr_en )-1;
 
 print_r( $arr_en );
 
@@ -21,5 +21,47 @@ foreach ($arr_en as $v)
     }
     continue;
 }
-    echo "Алексеєнко Оксана Олегівна " . $j1 . $j2 . $j3;
+    echo "Алексеєнко Оксана Олегівна " . $j1 . $j2 . $j3 . "<br>";
 
+
+//$name = $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)]. $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)];
+$name = $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] ;
+
+if ($name === "ok")
+{
+    echo  "Алексеєнко Оксана Олегівна -" . $name;
+}
+else
+{
+    echo $name . " - NO";
+}
+//Ура
+/*$arr_en = range( 'a', 'z' );
+$arr_en =['a', 'o' , 'k', 's', 'n'];
+$len    = count( $arr_en ) - 1;
+
+print_r( $arr_en );
+
+$found = false;
+$arr = ['a', 'o' , 'k', 's', 'n'];
+$i=0;
+while( !$found )
+{
+//    $arr = [];
+//
+//    for( $i = 1; $i < 7; $i++ )
+//    {
+//        $arr[] = $arr_en[ rand( 0, $len ) ];
+//    }
+//
+//    $found = true;
+    echo "try: <br>";
+    if ($arr[$i] === 'oks')
+    {
+        $found = true;
+        echo $arr[$i];
+    }
+    $i++;
+}
+
+//print_r( $arr );*/
