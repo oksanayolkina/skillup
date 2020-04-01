4327082369e1c693f5d731ea218f6f40e04afd5e
Oksana Alekseienko <oksana.alekseienko@gmail.com>
2020-04-01 11:58:32 +0300

    homework2: while

--- php_cycles.php
+++ php_cycles.php
@@ -1,67 +1,16 @@
 <?php
-
-$arr_en = range( 'a', 'z' );
-$len    = count( $arr_en )-1;
-
-print_r( $arr_en );
-
-foreach ($arr_en as $v)
-{
-    if ($v ==='a')
-    {
-       $j1 = 'a';
-    }
-    else if ($v === 'o')
-    {
-        $j2 = 'o';
-    }
-    else if ($v === 'b')  // вказую "b", тому що не знаю як зробити ще раз букву "о"
-    {
-        $j3 = 'b';
-    }
-    continue;
-}
-    echo "Алексеєнко Оксана Олегівна " . $j1 . $j2 . $j3 . "<br>";
-
-
-//$name = $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)]. $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)];
-$name = $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] ;
-
-if ($name === "ok")
-{
-    echo  "Алексеєнко Оксана Олегівна -" . $name;
-}
-else
-{
-    echo $name . " - NO";
-}
-//Ура
-/*$arr_en = range( 'a', 'z' );
-$arr_en =['a', 'o' , 'k', 's', 'n'];
+//$arr_en = range( 'a', 'z' );
+$arr_en = [ 'a', 'o', 'k', 's', 'n' ];
 $len    = count( $arr_en ) - 1;
 
-print_r( $arr_en );
+//print_r( $arr_en );
 
-$found = false;
-$arr = ['a', 'o' , 'k', 's', 'n'];
-$i=0;
-while( !$found )
+//$name = $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)] . $arr_en[rand(0,$len)];
+
+while( ( $name = $arr_en[ rand( 0, $len ) ] . $arr_en[ rand( 0, $len ) ] . $arr_en[ rand( 0, $len ) ] ) !== "aoo" )
 {
-//    $arr = [];
-//
-//    for( $i = 1; $i < 7; $i++ )
-//    {
-//        $arr[] = $arr_en[ rand( 0, $len ) ];
-//    }
-//
-//    $found = true;
-    echo "try: <br>";
-    if ($arr[$i] === 'oks')
-    {
-        $found = true;
-        echo $arr[$i];
-    }
-    $i++;
+    echo "$name is Not aoo <br>";
 }
+echo "FOUND $name";
 
-//print_r( $arr );*/
+die();
